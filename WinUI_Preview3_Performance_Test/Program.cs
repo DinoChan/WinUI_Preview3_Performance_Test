4f4a8732801505c6e967e21401d8c4dc609f91da
Jinhua Chen <Jinhua.Chen@omec-instruments.com>
2020-11-27 15:18:55 +0800

    添加项目文件。

---
+++ WinUI_Preview3_Performance_Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WinUI_Preview3_Performance_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
